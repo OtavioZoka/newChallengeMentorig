ba2be7e442412ece9894e959da511c7eeb8258ac
Otávio Reis Viana <otavio.viana@rocketmail.com>
2023-02-24 11:17:58 -0300

    atualização do webpack e typescript

--- readme.md
+++ readme.md
@@ -2,15 +2,22 @@ Passo a Passo:
 comandos no terminal
 
 - npm init -y
-- npm install react react-dom
-- npm install webpack webpack-cli webpack-dev-server html-webpack-plugin --save-dev
-- npm install @babel/core @babel/preset-env @babel/preset-react @babel/plugin-proposal-class-properties @babel/plugin-proposal-private-methods babel-loader --save-dev
+- npm i react react-dom --save
+- npm i webpack webpack-cli webpack-dev-server html-webpack-plugin --save-dev
+- npm i @babel/core @babel/preset-env @babel/preset-react babel-loader --save-dev
+- npm i typescript --save-dev
+- npm i @babel/preset-typescript --save-dev
+- npm i @babel/plugin-transform-runtime --save-dev
 
 Agora configurações<br>
 criar um arquivo de configuração do webpack com nome 'webpack.config.js' com suas devidas configurações<br>
 criar um arquivo de configuração do babel com nome '.babelrc' com suas devidas configurações
 
-criar a pasta 'src' na raiz do projeto
+Criar a pasta 'src' na raiz do projeto
 
-adicionar um arquivo 'App.js'
-adicionar um arquivo 'index.js'
+adicionar um arquivo 'App.tsx'
+adicionar um arquivo 'index.tsx'
+
+Criar a pasta public na raiz do projeto
+
+e passar o index.html aqui pra dentro.
--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,21 @@ const path = require( 'path' );
 const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
+    resolve: {
+        extensions: [ ".ts", '.tsx', '.js', '.jsx' ]
+    },
+    entry: './src/index.tsx',
     output: {
         path: path.resolve( __dirname, 'dist' ),
-        filename: 'bundle.js'
+        chunkFilename: "[id].[contenthash].js"
     },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.(ts|tsx|js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
-                    options: {
-                        presets: [ '@babel/preset-env', '@babel/preset-react' ],
-                        plugins: [
-                            [ '@babel/plugin-proposal-class-properties', { loose: true } ],
-                            [ '@babel/plugin-proposal-private-methods', { loose: true } ],
-                            [ '@babel/plugin-proposal-private-property-in-object', { loose: true } ]
-                        ]
-                    }
                 }
             }
         ]
@@ -30,7 +24,7 @@ module.exports = {
     ,
     plugins: [
         new HtmlWebpackPlugin( {
-            template: './src/index.html'
+            template: './public/index.html'
         } )
     ]
 };
